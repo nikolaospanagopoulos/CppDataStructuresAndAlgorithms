68c133e773fa2dac5914f059eb1150086a4ac635
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2023-09-28 15:36:04 +0300

    feat: algorithms

--- algorithms/arrays.cpp
+++ algorithms/arrays.cpp
@@ -52,15 +52,36 @@ void leftMax(int *arr, int len) {
 
   arr[len - 1] = std::max(arr[len - 1], arr[len - 2]);
 }
+int suffixSum(int *arr, int size, int elementNum) {
+  if (elementNum == 0) {
+    return 0;
+  }
+  if (size == 1) {
+    return arr[0];
+  }
+  return suffixSum(arr, size - 1, elementNum - 1) + arr[size - 1];
+}
+int prefixSum(int *arr, int size, int elementNum) {
+  if (elementNum == 0) {
+    return 0;
+  }
+  if (size == 1) {
+    return arr[0];
+  }
+  return prefixSum(arr, size - 1, elementNum - 1) + arr[elementNum - 1];
+}
+void rightMax(int *arr, int len, int position = 0) {
+  if (len == 1) {
+    return;
+  }
+  rightMax(arr, len - 1, position + 1);
 
+  arr[position] = std::max(arr[position], arr[position + 1]);
+}
 int main() {
   int arr[] = {200, 1000, 300, 600, 200, 600, 500, 800, 100, 800};
 
   std::cout << arrAvg(arr, 10) << std::endl;
 
-  leftMax(arr, 10);
-
-  for (int i = 0; i < 10; i++) {
-    std::cout << arr[i] << std::endl;
-  }
+  std::cout << prefixSum(arr, 10, 4) << std::endl;
 }
